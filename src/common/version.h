012ece12b686ad9ba380e42dc01a2d0e616618e7
Lost <lostincynicism@gmail.com>
2015-07-14 11:46:07 -0500

    Added Realtek's versioning

--- src/common/version.h
+++ src/common/version.h
@@ -5,6 +5,13 @@
 #define VERSION_STR_POSTFIX ""
 #endif /* VERSION_STR_POSTFIX */
 
-#define VERSION_STR "2.3" VERSION_STR_POSTFIX
+#include "rtw_version.h"
+
+#ifdef RTW_VERSION
+        #define VERSION_STR "2.3" VERSION_STR_POSTFIX "_" RTW_VERSION
+#else
+        #define VERSION_STR "2.3" VERSION_STR_POSTFIX
+#endif
 
 #endif /* VERSION_H */
+
